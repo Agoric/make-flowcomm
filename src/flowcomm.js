23300876d2b7729eda9bf0108358002253b89722
Dean Tribble <tribble@agoric.com>
2019-02-23 11:36:53 -0800

    Copy Flow source from PlaygroundVat
    github.com/Agoric/PlaygroundVat commit 26b2b2b4da3d397a6feb3ce76465529c903f06de

---
+++ src/flowcomm.js
@@ -0,0 +1,615 @@
+/* eslint-disable no-use-before-define */
+// Turning no-use-before-define off for this file, since there are many references to each other
+
+// A simple comm system using Flows for ordering
+
+'use strict';
+
+/* eslint-disable-next-line global-require, import/no-extraneous-dependencies */
+import harden from '@agoric/harden';
+import { insist, insistFn } from '../insist';
+
+// const debugLog = console.log;
+function debugLog() {} // disabled
+
+function isSymbol(x) {
+  return typeof x === 'symbol';
+}
+
+let debugNextID = 1;
+const debugToID = new WeakMap();
+function debugRegister(obj, _info = null) {
+  const debugID = debugNextID;
+  debugNextID += 1;
+  debugToID.set(obj, debugID);
+  debugLog('REGISTER ', debugID, obj);
+  return debugID;
+}
+function getDebugID(key) {
+  return debugToID.get(key);
+}
+
+// TODO what happens to the then result?
+function scheduleTodo(target, todo) {
+  debugLog('SCHEDULE ', getDebugID(todo), todo);
+  Promise.resolve(target).then(todo.onFulfill, todo.onReject);
+}
+
+// TODO
+/* eslint-disable-next-line no-unused-vars */
+function isBroken(_target) {
+  return false;
+}
+
+function debugLogDelivery(target, methodName) {
+  if (Object(target) === target) {
+    if (!Reflect.getOwnPropertyDescriptor(target, methodName)) {
+      // debugLog(`target[${methodName}] is missing for ${target}`);
+    }
+  } else if (typeof target !== 'string') {
+    // debugLog(`target IS WONKY: ${target}`);
+  }
+}
+
+function frozenRejection(res) {
+  // todo: we need to marshal these usefully
+  return Object.freeze(Promise.reject(Object.freeze(res)));
+}
+
+class PendingDelivery {
+  constructor(methodName, args, handler) {
+    this.methodName = methodName;
+    this.args = args;
+    this.handler = handler;
+    this.debugID = debugRegister(this);
+    // debugLog(`PendingDelivery[${which}] ${op}, ${args}`);
+
+    this.onFulfill = target => {
+      debugLogDelivery(target, methodName);
+      let res;
+      try {
+        res = target[methodName](...args);
+      } catch (reason) {
+        // debugLog(`@@@@####$$$$ ${methodName} $$$$####@@@@`);
+        debugLog(
+          `exception during delivery[${methodName}]`,
+          reason,
+          reason.stack,
+        );
+        res = frozenRejection(reason);
+      }
+      // handler shouldn't ever throw an exception, but just in case let's look
+      // at it separately
+      handler.resolve(res);
+    };
+
+    this.onReject = reason => {
+      // shallow freeze in case it is an exception
+      handler.resolve(frozenRejection(reason));
+    };
+
+    // console.log(`PendingDelivery[${which}] ${op}, ${args}`);
+  }
+
+  /* eslint-disable-next-line class-methods-use-this */
+  get isMessageSend() {
+    return true;
+  }
+}
+
+class PendingThen {
+  constructor(onFulfill, onReject, handler) {
+    this.debugID = debugNextID;
+    debugNextID += 1;
+    debugRegister(this);
+    insistFn(onFulfill);
+
+    this.debugTxt = onFulfill.toString();
+
+    this.onFulfill = target => {
+      debugLog('THEN ', getDebugID(this), this);
+      let res;
+      try {
+        res = onFulfill ? onFulfill(target) : target;
+      } catch (reason) {
+        res = frozenRejection(reason);
+      }
+      handler.resolve(res);
+    };
+
+    this.onReject = reason => {
+      let res;
+      try {
+        res = onReject ? onReject(reason) : frozenRejection(reason);
+      } catch (reason2) {
+        res = frozenRejection(reason2);
+      }
+      handler.resolve(res);
+    };
+  }
+
+  /* eslint-disable-next-line class-methods-use-this */
+  get isMessageSend() {
+    return false;
+  }
+}
+
+/**
+ * Among objects, all and only promises have handlers.
+ */
+
+class UnresolvedHandler {
+  constructor() {
+    this.forwardedTo = null;
+    this.blockedFlows = [];
+    this.debugID = debugRegister(this);
+  }
+
+  resolve(target) {
+    insist(!this.forwardedTo, 'Must be unresolved');
+    debugLog('RESOLVE ', getDebugID(target) || '*', target, 'this:', this);
+    const targetInner = getInnerVow(target);
+    // if the target is a vow, forward to it, otherwise
+    // target might be a Presence, or local object, or received pass-by-copy
+    // object. In that case fulfill to it.
+    return targetInner ? this.forwardTo(targetInner) : this.fulfill(target);
+  }
+
+  // Fulfill the vow. Reschedule any flows that were blocked on this vow.
+  fulfill(target) {
+    const rec = farVows.get(target);
+    const handler = rec ? rec.handler : new FulfilledHandler(target);
+    return this.directForward(handler);
+  }
+
+  // Fulfill the vow. Reschedule any flows that were blocked on this vow.
+  forwardTo(targetInner) {
+    const valueR = shortenForwards(targetInner.resolver, targetInner);
+    return this.directForward(valueR);
+  }
+
+  directForward(valueR) {
+    this.forwardedTo = valueR;
+    if (this.blockedFlows.length) {
+      // There are waiting flows; move them to the end of the chain
+      valueR.processBlockedFlows(this.blockedFlows);
+      this.blockedFlows = null;
+    }
+    return valueR;
+  }
+
+  processBlockedFlows(blockedFlows) {
+    // debugLog(`Appending blocked flow ${blockedFlows}`);
+
+    insist(!this.forwardedTo, 'INTERNAL: Must be unforwarded to accept flows.');
+    this.blockedFlows.push(...blockedFlows);
+  }
+
+  processSingle(todo, flow) {
+    debugLog('PROCESS unresolved ', getDebugID(todo), todo, this, flow);
+    // the target of the next message is unresolved so
+    // this flow is now waiting for shortTarget
+    this.blockedFlows.push(flow);
+    return false;
+  }
+}
+harden(UnresolvedHandler);
+
+class FulfilledHandler {
+  constructor(value) {
+    this.forwardedTo = null;
+    this.value = value;
+    this.debugID = debugRegister(this);
+  }
+
+  // Fulfill the vow. Reschedule any flows that were blocked on this vow.
+  /* eslint-disable-next-line class-methods-use-this */
+  fulfill(_value) {
+    insist(false, 'Fulfill only applies to unresolved promise');
+  }
+
+  // Fulfill the vow. Reschedule any flows that were blocked on this vow.
+  /* eslint-disable-next-line class-methods-use-this */
+  forwardTo_(_valueInner) {
+    insist(false, 'Forward only applies to unresolved promise');
+  }
+
+  /* eslint-disable-next-line class-methods-use-this */
+  processBlockedFlows(blockedFlows) {
+    blockedFlows.forEach(flow => {
+      // debugLog(`Processing blocked flow ${flow}`);
+      flow.scheduleUnblocked();
+    });
+  }
+
+  processSingle(todo, _flow) {
+    debugLog('PROCESS single ', getDebugID(todo), todo, this);
+    scheduleTodo(this.value, todo);
+    return true;
+  }
+}
+harden(FulfilledHandler);
+
+class FarRemoteHandler extends UnresolvedHandler {
+  constructor(serializer, vatID, swissnum, presence = null) {
+    super();
+    this.serializer = serializer;
+    this.vatID = vatID;
+    this.swissnum = swissnum;
+    this.value = presence; // note: other folks test for '.value', so don't rename it
+  }
+
+  // Unblock flows so that messages are delivered
+  // TODO: flow interation here must be fixed when we enforce ordering
+  processBlockedFlows(blockedFlows) {
+    if (this.value) {
+      blockedFlows.forEach(flow => {
+        flow.scheduleUnblocked();
+      });
+    } else {
+      return super.processBlockedFlows(blockedFlows);
+    }
+  }
+
+  processSingle(todo, flow) {
+    debugLog('PROCESS remote ', getDebugID(todo), todo, this);
+
+    if (todo.isMessageSend) {
+      const { methodName, args } = todo;
+      // the serializer gets private access to resolutionOf(), which it uses
+      // to build the right webkeys
+
+      // construct a new Vow for the result, pointing at a FarHandler to the
+      // same vat as our own Presence target
+
+      const resData = this.serializer.allocateSwissStuff();
+
+      // create a synthetic RemoteVow, as if we'd received swissnum from
+      // targetvat. We register it with the comms tables so that when the
+      // other end sends their {type:'resolve'} message, it will cause this
+      // resultVow to resolve, and any queued messages we put into it will be
+      // delivered. We choose the swissnum because we're allocating the
+      // object, but we do it with a swissbase so we can't deliberately
+      // collide with anything currently allocated on the other end
+
+      const resultVow = makeUnresolvedRemoteVow(
+        this.serializer,
+        this.vatID,
+        resData.swissnum,
+        flow,
+      );
+      debugLog('handlerOf(resultVow) = ', handlerOf(resultVow));
+      this.serializer.opSend(
+        resData.swissbase,
+        this.vatID,
+        this.swissnum,
+        methodName,
+        args,
+        resolutionOf,
+      );
+      this.serializer.registerRemoteVow(
+        this.vatID,
+        resData.swissnum,
+        resultVow,
+      );
+
+      todo.handler.resolve(resultVow);
+
+      return true;
+    }
+    if (this.value) {
+      // this is a then() on a RemoteVow, which should cause a round trip to
+      // flush all the previous messages, but doesn't actually target the
+      // specific object. todo: flow enforcement
+
+      // todo: opThen
+      // this.serializer.opThen(this.vatID, this.swissnum);
+
+      scheduleTodo(this.value, todo);
+      return true;
+    }
+    return super.processSingle(todo, flow);
+  }
+}
+harden(FarRemoteHandler);
+
+class InnerFlow {
+  constructor() {
+    // a queue of message structured as [targetR, op, args, resultR]
+    this.pending = [];
+  }
+
+  // add a message to the end of the flow
+  // todo shorten and clean up shorten
+  enqueue(innerVow, todo) {
+    // console.log('enqueue entering');
+    const firstR = innerVow.resolver;
+    const shortTarget = shortenForwards(firstR, innerVow);
+    if (this.pending.length === 0) {
+      // console.log(`InnerFlow.enqueue found an empty queue`);
+      // This will be the first pending action, so it's either ready to schedule or
+      // is what this flow will be waiting on
+      const processed = shortTarget.processSingle(todo, this);
+      if (processed) {
+        // fastpath; the action was scheduled immediately since it was ready and the flow was empty
+        // console.log(`InnerFlow.enqueue exiting on fast path`);
+        return;
+      }
+    }
+    this.pending.push([shortTarget, todo]);
+    // console.log(`InnerFlow.enqueue exiting with ${this.pending.length} entries`);
+    // console.log(`  e[0] is ${whichTodo.get(this.pending[0][1])}`);
+  }
+
+  // The blocking resolver has been resolved. Schedule all unlocked pending flows, in order
+  scheduleUnblocked() {
+    // TODO add assertion that this always finds at least one resolved promise
+    while (this.pending.length > 0) {
+      const msg = this.pending[0];
+      const [target, todo] = msg;
+      const shortTarget = shortenForwards(target);
+      const processed = shortTarget.processSingle(todo, this);
+      if (processed) {
+        // The todo was processed; remove it from pending
+        this.pending.shift();
+      } else {
+        // the target of the next message is unresolved, so break
+        break;
+      }
+    }
+  }
+
+  toStringX() {
+    return `Flow {
+      pending: ${this.pending.join('  \n')}
+    }`;
+  }
+}
+harden(InnerFlow);
+
+const flowToInner = new WeakMap();
+
+function realInnerFlow(value) {
+  const result = flowToInner.get(value);
+  insist(result, 'Valid instance required');
+  return result;
+}
+
+const farVows = new WeakMap(); // maps Presence to { vatID, swissnum, handler }
+
+export function makePresence(serializer, vatID, swissnum) {
+  const presence = harden({});
+  const handler = new FarRemoteHandler(serializer, vatID, swissnum, presence);
+  const rec = { vatID, swissnum, handler };
+  farVows.set(presence, rec);
+  return presence;
+}
+
+export function makeUnresolvedRemoteVow(
+  serializer,
+  vatID,
+  swissnum,
+  flow = new InnerFlow(),
+) {
+  const handler = new FarRemoteHandler(serializer, vatID, swissnum);
+  debugLog(`makeUnresolvedRemoteVow: ${vatID}/${swissnum}`);
+  return new Vow(flow, handler);
+}
+
+class Flow {
+  constructor() {
+    flowToInner.set(this, new InnerFlow());
+  }
+
+  makeVow(resolveFn) {
+    const flow = realInnerFlow(this);
+    const innerResolver = new UnresolvedHandler();
+    const resultR = makeResolver(innerResolver);
+    resolveFn(resultR);
+    return new Vow(flow, innerResolver);
+  }
+}
+harden(Flow);
+
+// follow a chain of handlers
+function shortenForwards(firstResolver, optVow) {
+  let nextR = firstResolver;
+  let lastR;
+  do {
+    lastR = nextR;
+    nextR = lastR.forwardedTo;
+  } while (nextR);
+  if (lastR !== firstResolver) {
+    // there's a chain to shorten; start from the front again and make all nodes forward to the end of the chain
+    let h = firstResolver;
+    do {
+      const k = h.forwardedTo;
+      h.forwardedTo = lastR;
+      h = k;
+    } while (h !== lastR);
+    if (optVow) {
+      optVow.resolver = lastR;
+    }
+  }
+  return lastR;
+}
+
+function makeResolver(innerResolver) {
+  const resolver = value => {
+    // TODO how do we detect cycles
+    // TODO use 'this' for the identity
+    innerResolver = innerResolver.resolve(value);
+  };
+  // resolver.toString = () => `Resolver{ resolved: ${getHandler(resolver)} }`;
+  return harden(resolver);
+}
+harden(makeResolver);
+
+const vowToInner = new WeakMap();
+const resolverToInner = new WeakMap();
+
+// TODO change to throw TypeError if these aren't present.
+function validInnerResolver(value) {
+  const result = resolverToInner.get(value);
+  insist(result, 'Valid instance required');
+  return result;
+}
+
+function getInnerVow(value) {
+  return vowToInner.get(value);
+}
+
+export function resolutionOf(value) {
+  const inner = getInnerVow(value);
+  if (!inner) {
+    return undefined;
+  }
+  const firstR = inner.resolver;
+  const shortHandler = shortenForwards(firstR, inner);
+  return shortHandler.value;
+}
+
+export function handlerOf(value) {
+  const inner = getInnerVow(value);
+  if (!inner) {
+    return undefined;
+  }
+  debugLog('handlerOf says inner is ', inner);
+  const firstR = inner.resolver;
+  return shortenForwards(firstR, inner);
+}
+
+export function isVow(value) {
+  return vowToInner.has(value);
+}
+
+function validVow(value) {
+  const result = vowToInner.get(value);
+  insist(result, 'Valid instance required');
+  return result;
+}
+
+class InnerVow {
+  constructor(innerFlow, innerResolver) {
+    this.flow = innerFlow;
+    this.resolver = innerResolver;
+    // harden(this);
+  }
+
+  get(target, op, receiver) {
+    return isSymbol(op)
+      ? Reflect.get(target, op, receiver)
+      : (...args) => {
+          const handler = new UnresolvedHandler();
+          // TODO don't make an outer resolver
+          const resultR = makeResolver(handler);
+          this.flow.enqueue(this, new PendingDelivery(op, args, handler));
+          /* eslint-disable-next-line no-constant-condition */
+          if (0) {
+            // ordering hack, want to remove this
+            return new Vow(this.flow, handler);
+          }
+          return new Vow(new InnerFlow(), handler);
+        };
+  }
+
+  /* eslint-disable-next-line class-methods-use-this */
+  getOwnPropertyDescriptor(_target, _name, _receiver) {
+    // debugLog(`GET ${typeof prop === 'symbol' ? prop.toString() : prop}`);
+  }
+
+  enqueueThen(onFulfill, onReject) {
+    const handler = new UnresolvedHandler();
+    this.flow.enqueue(this, new PendingThen(onFulfill, onReject, handler));
+    /* eslint-disable-next-line no-constant-condition */
+    if (0) {
+      // didn't need the ordering hack here, not sure why
+      return new Vow(this.flow, handler);
+    }
+    return new Vow(new InnerFlow(), handler);
+  }
+}
+
+class Vow {
+  // TODO move the constructor out
+  constructor(innerFlow, innerResolver) {
+    debugRegister(this);
+    const inner = new InnerVow(innerFlow, innerResolver);
+    vowToInner.set(this, inner);
+    // if .e were enumerable, JSON serialization would recurse forever, which
+    // makes debugging annoying
+    Object.defineProperty(this, 'e', {
+      value: new Proxy({}, inner),
+      enumerable: false,
+    });
+    // harden(this);
+  }
+
+  // TODO need second argument for `then`
+  then(onFulfill, onReject) {
+    const inner = validVow(this);
+    return inner.enqueueThen(onFulfill, onReject);
+  }
+
+  fork() {
+    const old = validVow(this);
+    const f = new InnerFlow();
+    return new Vow(f, old.resolver);
+  }
+
+  static all(answerPs) {
+    let countDown = answerPs.length;
+    const answers = [];
+    if (countDown === 0) {
+      return Vow.resolve(answers);
+    }
+    return new Flow().makeVow(resolve => {
+      answerPs.forEach((answerP, index) => {
+        Vow.resolve(answerP).then(answer => {
+          answers[index] = answer;
+          countDown -= 1;
+          if (countDown === 0) {
+            resolve(answers);
+          }
+        });
+      });
+    });
+  }
+
+  static join(xP, yP) {
+    return Vow.all([xP, yP]).then(([x, y]) => {
+      if (Object.is(x, y)) {
+        return x;
+      }
+      throw new Error('not the same');
+    });
+  }
+
+  static race(answerPs) {
+    return new Flow().makeVow((resolve, reject) => {
+      answerPs.forEach(answerP => {
+        Vow.resolve(answerP).then(resolve, reject);
+      });
+    });
+  }
+
+  static resolve(val) {
+    if (isVow(val)) {
+      return val;
+    }
+    // todo this could be more efficient by looking at farVows[val] and
+    // grabbing the handler directly
+    const f = new Flow();
+    return f.makeVow((resolve, _reject) => resolve(val));
+  }
+
+  static fromFn(fn) {
+    return Vow.resolve().then(() => fn());
+  }
+}
+harden(Vow);
+
+const asVow = Vow.resolve;
+
+export { Flow, Vow, makeResolver, asVow };
+export default Flow;
---
+++ test/test-flow.js
@@ -0,0 +1,203 @@
+import { test } from 'tape-promise/tape';
+
+import { Flow, makeUnresolvedRemoteVow } from '../src/flow/flowcomm';
+
+test('tape works', t => {
+  t.equal(1, 1);
+  t.end();
+});
+
+function delay(fn) {
+  Promise.resolve(null).then(fn);
+}
+
+// TODO: move most of the Promises from this file into a single utility
+// function which schedules a new turn
+
+test('async tests pass', async t => {
+  const a = await Promise.resolve(42);
+  t.equal(a, 42);
+  t.end();
+});
+
+test('unresolved send queues in order', async t => {
+  const f1 = new Flow();
+  let r1;
+  const v1 = f1.makeVow(r => (r1 = r));
+
+  // const v2 = v1.e.concat(" MORE"); //v1 ! concat(" MORE")
+  const v2 = v1.e.concat(' MORE'); // v1!concat(" MORE")
+
+  delay(() => r1('some'));
+
+  const res = await v2;
+
+  t.equal(res, 'some MORE');
+
+  t.end();
+});
+
+test('resolved send queues in order', async t => {
+  const f1 = new Flow();
+  let r1;
+  const v1 = f1.makeVow(r => (r1 = r));
+  delay(() => r1('some'));
+  const v2 = v1.e.concat(' MORE'); // v1 ! concat(" MORE")
+  const res = await v2;
+  t.equal(res, 'some MORE');
+  t.end();
+});
+
+test('pre-resolved send queues in order', async t => {
+  const f1 = new Flow();
+  let r1;
+  const v1 = f1.makeVow(r => (r1 = r));
+  r1('some');
+  const v2 = v1.e.concat(' MORE'); // v1 ! concat(" MORE")
+  const res = await v2;
+  t.equal(res, 'some MORE');
+  t.end();
+});
+
+test('order across forwarding', async t => {
+  let c = 0;
+  console.log(`s ${(c += 1)}`);
+  const f1 = new Flow();
+  let r1;
+  const v1 = f1.makeVow(r => (r1 = r));
+  const v2 = v1.e.concat(' MORE'); // v1 ! concat(" MORE")
+  console.log(`s ${(c += 1)}`);
+  let r3;
+  const v3 = f1.makeVow(r => (r3 = r));
+  r1(v3);
+  r3('some');
+  console.log(`s ${(c += 1)}`);
+
+  const res = await v2;
+  console.log(`s ${(c += 1)}`);
+  t.equal(res, 'some MORE');
+  t.end();
+});
+
+test('all flow', t => {
+  const f1 = new Flow();
+  let r1;
+  const v1 = f1.makeVow(r => (r1 = r));
+  /* eslint-disable-next-line no-unused-vars */
+  let r2;
+  const x1 = f1.makeVow(r => (r2 = r));
+
+  const v2 = v1.e.concat(' MORE'); // v1 ! concat(" MORE")
+  delay(() => r1('some'));
+  // console.log(v1);
+
+  v1.then(s => console.log(`THE      N1 ${s}`));
+  v2.then(s => console.log(`THEN2 ${s}`));
+
+  /* eslint-disable-next-line no-unused-vars */
+  const x2 = x1.e.concat(' ANOTHER'); // x1 ! concat(" ANOTHER")
+
+  const v3 = v1.e.split();
+  // console.log(v3);
+
+  v3.then(s => console.log(`THEN3 ${s}`));
+
+  console.log(`${f1}`);
+
+  Promise.resolve('DONE').then(console.log);
+
+  t.end();
+});
+
+test('remote vow', t => {
+  const results = [];
+  const serializer = {
+    allocateSwissStuff() {
+      return { swissbase: 'base1', swissnum: 'num1' };
+    },
+    registerRemoteVow(targetVatID, swissnum, val) {
+      console.log(`registerRemoteVow: ${targetVatID}, ${swissnum}, ${val}`);
+    },
+    opSend(
+      resultSwissbase,
+      targetVatID,
+      targetSwissnum,
+      methodName,
+      args,
+      _resolutionOf,
+    ) {
+      results.push({
+        resultSwissbase,
+        targetVatID,
+        targetSwissnum,
+        methodName,
+        args,
+      });
+    },
+  };
+  const v1 = makeUnresolvedRemoteVow(serializer, 'vat1', 'swiss1');
+  v1.e.foo('arg1', 'arg2');
+
+  t.deepEqual(results, [
+    {
+      resultSwissbase: 'base1',
+      targetVatID: 'vat1',
+      targetSwissnum: 'swiss1',
+      methodName: 'foo',
+      args: ['arg1', 'arg2'],
+    },
+  ]);
+
+  t.end();
+});
+
+test('JSON serialize Vow', t => {
+  const f1 = new Flow();
+  /* eslint-disable-next-line no-unused-vars */
+  let r1;
+  const v1 = f1.makeVow(r => (r1 = r));
+  // this used to suffer infinite recursion and overflowed the stack
+  t.equal(`${JSON.stringify(v1)}`, '{}');
+  t.end();
+});
+
+test('simple broken vow', async t => {
+  const f1 = new Flow();
+  let r1;
+  const v1 = f1.makeVow(r => (r1 = r));
+  const v2 = v1.e.badMessage(' MORE'); // v1!badMessage(" MORE")
+
+  delay(() => r1('some'));
+  let res;
+  try {
+    res = await v2;
+  } catch (reason) {
+    res = 42;
+  }
+  t.equal(res, res);
+  t.end();
+});
+
+test('error across forwarding', async t => {
+  let c = 0;
+  console.log(`s ${(c += 1)}`);
+  const f1 = new Flow();
+  let r1;
+  const v1 = f1.makeVow(r => (r1 = r));
+  const v2 = v1.e.concat(' MORE'); // v1 ! concat(" MORE")
+  console.log(`s ${(c += 1)}`);
+  let r3;
+  const v3 = f1.makeVow(r => (r3 = r));
+  r1(v3);
+  r3(7);
+  console.log(`s ${(c += 1)}`);
+
+  let res;
+  try {
+    res = await v2;
+  } catch (reason) {
+    res = 42;
+  }
+  t.equal(res, res);
+  t.end();
+});
